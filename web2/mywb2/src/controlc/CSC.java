271264d45ae2c301fea17f289be6cf56bddbb39f
owner <mashevit@gmail.com>
2018-07-16 18:21:09 +0300

    tst
    tst

--- web2/mywb2/src/controlc/CSC.java
+++ web2/mywb2/src/controlc/CSC.java
@@ -50,7 +50,7 @@ public class CSC extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
-	 */
+	 *///test
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
